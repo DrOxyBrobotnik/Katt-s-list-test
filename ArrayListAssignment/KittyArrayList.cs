71a36dc5a002aa556e8f607a2ae72fc5fc129d44
DrOxyBrobotnik <120782522+DrOxyBrobotnik@users.noreply.github.com>
2023-03-20 13:52:24 +0100

    Working on assignment

--- ArrayListAssignment/KittyArrayList.cs
+++ ArrayListAssignment/KittyArrayList.cs
@@ -2,6 +2,42 @@
 {
     public class KittyArrayList
     {
+        public string Length { get; set; }
+        KittyArrayList(int dumma)
+        {
 
+        }
+
+        KittyArrayList(int dumma, int bunna)
+        {
+
+        }
+
+        KittyArrayList(int dumma, int bunna, int cumma)
+        {
+
+        }
+
+        int[] bunny = { 0 };
+        int[] katt = { 0, 0 };
+        int Get(int dumma)
+        {
+            return dumma;
+        }
+
+        void Add(int bunna)
+        {
+            bunna = bunny;
+            bunna = katt;
+        }
+
+        void Remove(int cumma)
+        {
+            cumma = 0;
+            if (bunny || katt.Contains(cumma))
+            {
+                Console.WriteLine("Sorry, number deleted");
+            }
+        }
     }
 }
